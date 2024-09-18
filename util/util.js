4a34a037b4795be997efff1c4fb00566ec9bb538
An Nguyen <cuvitdet15@gmail.com>
2024-09-18 16:37:56 +0700

    update

--- util/util.js
+++ util/util.js
@@ -13,13 +13,6 @@ import axios from "axios";
  export async function filterImageFromURL(inputURL) {
   return new Promise(async (resolve, reject) => {
     try {
-
-      // const res = await axios({
-      //   url: inputURL,
-      //   method: 'GET',
-      //   responseType: 'arraybuffer',
-      // })
-
       const photo = await Jimp.read(inputURL);
       const outpath =
         "/tmp/filtered." + Math.floor(Math.random() * 2000) + ".jpg";
